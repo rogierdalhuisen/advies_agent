fd4e61d05289d0f6c5f65364d7284bef00cbd1cc
rogierdalhuisen <rogierdalhuisen@gmail.com>
2026-01-29 16:43:45 +0100

    added data prep t databse

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,3 +1,8 @@
 """Database module for AdviesAanvragen."""
 
 from src.database.repository import get_all, get_recent_24h, get_by_email
+from src.database.data_preprocessor import (
+    PreprocessedUser,
+    preprocess_user,
+    preprocess_all_users,
+)
