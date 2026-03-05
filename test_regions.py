00eb4b6a770126be58aaea84b210167b5c7c9e50
rogierdalhuisen <rogierdalhuisen@gmail.com>
2026-03-05 13:54:12 +0100

    removed

--- test_regions.py
+++
@@ -1,8 +0,0 @@
-import sys
-from src.database.data_preprocessor import preprocess_user
-from src.graph.orchestrator_graph.preprocessing import prepare_orchestrator_input
-
-user = preprocess_user(92)
-print("bestemming_land:", user.bestemming_land)
-inputs = prepare_orchestrator_input(user)
-print("regions:", inputs['regions'])
